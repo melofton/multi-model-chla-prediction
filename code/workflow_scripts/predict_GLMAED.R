615dadffbf0456c5efe035305906d12a00140bbd
Mary Elizabeth Lofton <melofton@tc001.cm.cluster>
2024-05-15 08:16:51 -0400

    rerunning predictions for glm-aed

--- code/workflow_scripts/predict_GLMAED.R
+++ code/workflow_scripts/predict_GLMAED.R
@@ -16,10 +16,10 @@ dat_GLMAED <- read_csv("./data/data_processed/GLMAED.csv")
 
 pred_GLMAED <- GLMAED(spinup_folder = "./code/model_files/GLM-AED/spinup",
                       prediction_folder = "./code/model_files/GLM-AED/prediction",
-                      rerun_spinup = FALSE,
+                      rerun_spinup = TRUE,
                       spinup_dates = c('2018-04-20 12:00:00','2022-01-01 12:00:00'),
-                      start_from_spinup = FALSE,
-                      pred_dates = seq.Date(from = as.Date("2022-10-02"), to = as.Date("2023-11-26"), by = "day"),
+                      start_from_spinup = TRUE,
+                      pred_dates = seq.Date(from = as.Date("2022-01-01"), to = as.Date("2023-11-26"), by = "day"),
                       forecast_horizon = 35,
                       wq_vars = c('OXY_oxy','CAR_dic','CAR_pH','CAR_ch4','SIL_rsi','NIT_amm','NIT_nit','PHS_frp','OGM_doc','OGM_poc','OGM_don','OGM_pon','OGM_dop','OGM_pop','OGM_docr','OGM_donr','OGM_dopr','OGM_cpom','PHY_hot','PHY_cold','PHY_Nfixer'),
                       data = dat_GLMAED,
