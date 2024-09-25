2e6bfe3056e6b884924c8585df212b106db285c2
Mary Elizabeth Lofton <melofton@tc040.cm.cluster>
2024-09-25 10:38:40 -0400

    checking how "off" the ic updating is

--- code/workflow_scripts/predict_GLMAED.R
+++ code/workflow_scripts/predict_GLMAED.R
@@ -5,8 +5,6 @@
 #Purpose: Predict chl-a in Falling Creek Reservoir in 2022-2023 using a suite of models
 #1-7 days into the future
 
-# TEST COMMIT
-
 library(tidyverse)
 library(lubridate)
 
@@ -17,11 +15,11 @@ source("./code/function_library/predict/GLMAED.R")
 dat_GLMAED <- read_csv("./data/data_processed/GLMAED.csv")
 
 pred_GLMAED <- GLMAED(spinup_folder = "./code/model_files/GLM-AED/spinup",
-                      prediction_folder = "./code/model_files/GLM-AED/prediction_test_100xOGM_docr",
+                      prediction_folder = "./code/model_files/GLM-AED/prediction",
                       rerun_spinup = TRUE,
-                      spinup_dates = c('2018-04-20 12:00:00','2022-01-01 12:00:00'),
+                      spinup_dates = c('2018-04-20 12:00:00','2021-12-29 12:00:00'),
                       start_from_spinup = TRUE,
-                      pred_dates = seq.Date(from = as.Date("2022-01-01"), to = as.Date("2023-11-26"), by = "day"),
+                      pred_dates = seq.Date(from = as.Date("2021-12-29"), to = as.Date("2022-01-07"), by = "day"),
                       forecast_horizon = 35,
                       wq_vars = c('OXY_oxy','CAR_dic','CAR_pH','CAR_ch4','SIL_rsi','NIT_amm','NIT_nit','PHS_frp','OGM_doc','OGM_poc','OGM_don','OGM_pon','OGM_dop','OGM_pop','OGM_docr','OGM_donr','OGM_dopr','OGM_cpom','PHY_hot','PHY_cold','PHY_Nfixer'),
                       data = dat_GLMAED,
