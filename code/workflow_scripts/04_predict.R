5ef0b95f32eb40c114ec4f846ba0d7309609c063
Mary Lofton <melofton@vt.edu>
2025-02-25 14:06:31 -0500

    running KGML model to predict observations

--- code/workflow_scripts/04_predict.R
+++ code/workflow_scripts/04_predict.R
@@ -200,12 +200,12 @@ pred_NNETAR_KGML <- fableNNETAR_KGML(previous_residuals = dat_NNETAR_KGML,
 pred_NNETAR_KGML2 <- fableNNETAR_KGML2(data = dat_NNETAR_KGML2,
                                        pred_dates = seq.Date(from = as.Date("2023-01-01"), to = as.Date("2023-11-26"), by = "day"),
                                        forecast_horizon = 35,
-                                       target = "residuals")
+                                       target = "observations")
 
 #OR if you only want to run one model
 mod_output <- read_csv("./model_output/validation_output.csv") %>%
-  filter(!model_id == "NNETAR_KGML") %>%
-  bind_rows(.,pred_TSLM_noLag) 
+  #filter(!model_id == "NNETAR_KGML") %>%
+  bind_rows(.,pred_NNETAR_KGML2) 
 
 unique(mod_output$model_id)
 
